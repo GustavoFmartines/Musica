0ab1bcdbfe480a2f718235610ee204f5c877a6fb
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-03-15 04:20:30 -0300

    Foi criado o código para exibir uma letra de musica

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+Console.WriteLine(@"
+Summer Nights
+Siamés
+");
+
+Console.ResetColor();
+
+Console.WriteLine(@"
+Tell her when it rains
+I don't feel it
+Tell her I'm to blame
+Didn't see it
+");
+
+Console.WriteLine(@"
+Winter's snows
+Don't hide my woes
+I can't lie to you
+I tried to warn her that sometimes we fall in the dark
+");
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+Console.WriteLine(@"
+It's summer time
+Singing Al Green in your car
+Heading to a party
+And the night air feels alive
+");
+
+Console.ResetColor();
+
+Console.WriteLine(@"
+Just sitting here
+I'm waiting, oh, yeah
+And he's not by my side
+Feeling dazed and I can't get him out
+Stuck in yesterday
+");
+
+Console.WriteLine(@"
+He don't know it
+I rarely show it
+I can't lie to you
+I tried to warn him that sometimes we fall in the dark
+");
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+Console.WriteLine(@"
+It's summer time
+Singing Al Green in your car
+Heading to a party
+And the night air feels alive
+But in my life
+I never meant to cause no harm
+Never hurt nobody
+Just wanna take this chance to fly
+On this summer night
+");
+
+Console.ResetColor();
+
+Console.WriteLine(@"
+But in my life
+I never meant to cause no harm
+Never hurt nobody
+Just wanna take this chance to fly
+");
+
+Console.ResetColor();
